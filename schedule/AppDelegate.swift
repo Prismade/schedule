edabacbff8ba8a199d193ed4632e8de074a43a7e
Prismade <selar.prismade@hotmail.com>
2020-01-19 19:48:51 +0300

    Tidied up process of launch and list of settings

--- schedule/AppDelegate.swift
+++ schedule/AppDelegate.swift
@@ -1,20 +1,30 @@
 import UIKit
-import CoreData
 
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-
-        let defaults = UserDefaults.standard
-        defaults.set(false, forKey: "firstRun")
-        defaults.set(4930, forKey: "group")
-
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        if UserDefaults.standard.object(forKey: "EnableCaching") == nil {
+            UserDefaults.standard.set(false, forKey: "EnableCaching")
+        }
+        
+        if UserDefaults.standard.object(forKey: "CacheExpirationPeriod") == nil {
+            UserDefaults.standard.set(604800, forKey: "CacheExpirationPeriod")
+        }
+        
+        if UserDefaults.standard.object(forKey: "ExtendedView") == nil {
+            UserDefaults.standard.set(false, forKey: "ExtendedView")
+        }
+        
+        if UserDefaults.standard.object(forKey: "ScrollOnStart") == nil {
+            UserDefaults.standard.set(false, forKey: "ScrollOnStart")
+        }
+        
+        if UserDefaults.standard.object(forKey: "SwipeToSwitch") == nil {
+            UserDefaults.standard.set(true, forKey: "SwipeToSwitch")
+        }
+        
         return true
     }
 
@@ -22,16 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
 
