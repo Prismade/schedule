2256b14e62cf23ac46d4a66fa548f0a741d919e1
Prismade <selar.prismade@hotmail.com>
2020-02-17 17:16:23 +0300

    Made Employee createable without Encodind/Decoding

--- schedule/Employee.swift
+++ schedule/Employee.swift
@@ -3,9 +3,35 @@ import Foundation
 
 final class Employee: Codable {
     final class Contacts: Codable {
+        init(address: String? = nil,
+             email: String? = nil,
+             phone: String? = nil) {
+
+            self.address = address
+            self.email = email
+            self.phone = phone
+        }
+
         let address: String?
         let email: String?
         let phone: String?
+
+
+    }
+    
+    init(id: Int, name: String,
+         image: String? = nil,
+         position: [String]? = nil,
+         degree: String? = nil,
+         rank: String? = nil,
+         contacts: Contacts) {
+        self.id = id
+        self.name = name
+        self.image = image
+        self.position = position
+        self.degree = degree
+        self.rank = rank
+        self.contacts = contacts
     }
     
     let id: Int
@@ -15,4 +41,6 @@ final class Employee: Codable {
     let degree: String?
     let rank: String?
     let contacts: Contacts
+
+
 }
