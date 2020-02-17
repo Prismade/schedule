bc2076756382d391038acb77564aac3ecc3c1f55
Prismade <selar.prismade@hotmail.com>
2020-02-17 18:03:46 +0300

    Repaired unit tests target. Made a test for EmployeeManager to check if it works.

---
+++ scheduleTests/EmployeeManagerTests.swift
@@ -0,0 +1,26 @@
+import XCTest
+@testable import Расписание
+
+
+class EmployeeManagerTests: XCTestCase {
+
+    func testParsing() {
+        
+        let expectation = XCTestExpectation(description: "Download apple.com home page")
+        
+        EmployeeManager.shared.data(for: 159) { result in
+            switch result {
+            case .success(let employee):
+                print(employee.name)
+                print(employee.image ?? "nil image")
+                expectation.fulfill()
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        
+        wait(for: [expectation], timeout: 10.0)
+    }
+
+
+}
