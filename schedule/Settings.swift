56f638ee23ff7965fa93da5a4c663f9664569814
Prismade <selar.prismade@hotmail.com>
2020-03-31 11:44:51 +0300

    Removed "Swipe to switch" option in settings

--- schedule/Settings.swift
+++ schedule/Settings.swift
@@ -6,7 +6,6 @@ import EventKitUI
 final class SettingsTableViewController: UITableViewController {
     
     @IBOutlet weak var scrollToTodaySwitch: UISwitch!
-    @IBOutlet weak var swipeToChangeSwitch: UISwitch!
     
     private lazy var calendarSelectionViewController = UINavigationController()
     
@@ -14,15 +13,12 @@ final class SettingsTableViewController: UITableViewController {
         super.viewDidLoad()
         scrollToTodaySwitch.isOn = UserDefaults.standard.bool(forKey: "ScrollOnStart")
         scrollToTodaySwitch.addTarget(self, action: #selector(onSwitchToggle(sender:)), for: .valueChanged)
-        swipeToChangeSwitch.isOn = UserDefaults.standard.bool(forKey: "SwipeToSwitch")
-        swipeToChangeSwitch.addTarget(self, action: #selector(onSwitchToggle(sender:)), for: .valueChanged)
         clearsSelectionOnViewWillAppear = true
     }
     
     @objc func onSwitchToggle(sender: UISwitch) {
         switch (sender.tag) {
             case 2: UserDefaults.standard.set(sender.isOn, forKey: "ScrollOnStart")
-            case 3: UserDefaults.standard.set(sender.isOn, forKey: "SwipeWeek")
             default: return
         }
     }
